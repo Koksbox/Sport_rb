2e8ad3d900338e0ba36dcf890f2fc45122c8206f
Azamat225 <azamat_rakhmatullin@bk.ru>
2026-01-18 12:18:46 +0500

    все бд, миграции и так далее тут

--- apps/users/api/urls.py
+++ apps/users/api/urls.py
@@ -1,7 +1,8 @@
 # apps/users/api/urls.py
 from django.urls import path
-from .views import select_role
+from .views import select_role, complete_profile
 
 urlpatterns = [
     path('select-role/', select_role, name='user-select-role'),
+    path('complete-profile/', complete_profile, name='user-complete-profile'),
 ]
